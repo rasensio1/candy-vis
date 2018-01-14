261574c009c3254fc5c006cdf60a5af521db7ed2
Ryan Asensio <ryan@Ryans-MacBook-Air.local>
2018-01-13 18:23:10 -0600

    WIP setting up initial state

--- src/candy_vis/core.cljs
+++ src/candy_vis/core.cljs
@@ -6,4 +6,24 @@
   (.getContext
     (.getElementById js/document "target") "2d"))
 
-(.fillRect context 0 0 20 20)
+(defonce children 15)
+(defonce canvas-width 800)
+(defonce canvas-height 800)
+
+(defonce candy-width-offset 3)
+(defonce candy-height (quot canvas-height children))
+(def candy-width (quot (- 800 (* children candy-width-offset )) children))
+
+(def initial-state {:rankings [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15]
+                    :candies (vec (repeat children 0))})
+
+
+
+
+;; GAME PLAN
+;; First, solve a hard-coded setup. Black blocks
+;; Take input (#children, each grade) and solve
+
+;; NEXT??
+;; multiple algorithms
+;; more problems?
