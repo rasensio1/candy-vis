538f0b09fac025a32a0a91346ebe8229e140cb42
Ryan Asensio <ryan.asensio@gmail.com>
2018-01-16 17:07:35 -0600

    Refactor and add tests

--- test/candy_vis/solve-test.cljs
+++ test/candy_vis/solve-test.cljs
@@ -9,12 +9,6 @@
                   :candies [1 1 1]
                   :ranks [1 2 1]})
 
-(def complex-state {:index 0
-                    :dir 1
-                    :finished false
-                    :candies [1 1 1 1 1 1 1 1 1 1]
-                    :ranks [2 4 2 6 1 7 8 9 2 1] })
-
 (defn solver [state]
   (let [app-state (atom state)]
     (while (false? (:finished @app-state)) (swap! app-state merge (sol/next-step @app-state)))
@@ -44,10 +38,18 @@
             } (solver dummy-state))))
 
   (testing "Can solve a complex situation"
-    (is (= [1 2 1 2 1 2 3 4 2 1] (:candies (solver complex-state))))
-    
-    )
+    (is (= [1 2 1 2 1 2 3 4 2 1] (:candies (solver (assoc dummy-state
+                                                    :candies [1 1 1 1 1 1 1 1 1 1]
+                                                    :ranks [2 4 2 6 1 7 8 9 2 1])))))) 
+  (testing "Can solve always increasing"
+    (is (= [1 2 3 4 5 6 7 8] (:candies (solver (assoc dummy-state 
+                                                      :ranks [1 2 3 4 5 6 7 8]
+                                                      :candies [1 1 1 1 1 1 1 1])))))) 
 
+  (testing "Can solve always decreasing"
+    (is (= [8 7 6 5 4 3 2 1] (:candies (solver (assoc dummy-state 
+                                                      :ranks [8 7 6 5 4 3 2 1]
+                                                      :candies [1 1 1 1 1 1 1 1]))))))
   
   )
 
